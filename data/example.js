a38ab6b18ff79562fcc0b72cea5ad66ce4846100
quarterblue <quarterslate@gmail.com>
2021-05-29 07:56:14 -0700

    Initialize frontend and data folder

---
+++ data/example.js
@@ -0,0 +1,14 @@
+const clubs = [
+  'Manchester City',
+  'Manchester United',
+  'Liverpool',
+  'Chelsea',
+  'Leicester City',
+  'West Ham',
+  'Tottenham Hotspurs',
+  'Arsenal',
+  'Leeds United',
+  'Everton',
+];
+
+module.exports = clubs;
